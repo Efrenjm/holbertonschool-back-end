c908ef7532f0d104f9c9a013ce90f8411f479cf3
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-04-19 20:16:08 -0600

    first task

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
         if todo.get("completed"):
             completed.append(todo.get("title"))
 
-    print(f"Employee {user.get('name')} is done with tasks({len(completed)}/{len(todos)}):")
+    print(f"Employee {user.get('name')} is done with tasks"
+          f"({len(completed)}/{len(todos)}):")
     for task in completed:
         print(f"\t{task}")
