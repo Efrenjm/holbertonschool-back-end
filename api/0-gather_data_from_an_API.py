43bbdc16a6bb17f2c4e2f0a223dd30c15f21baa8
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-04-19 20:22:31 -0600

    add space between tabs

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -21,4 +21,4 @@ if __name__ == "__main__":
     print(f"Employee {user.get('name')} is done with tasks"
           f"({len(completed)}/{len(todos)}):")
     for task in completed:
-        print(f"\t{task}")
+        print(f"\t {task}")
