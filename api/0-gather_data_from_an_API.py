55a216f6ab4382a81d7afaaffd2d527ae84e517a
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-04-19 20:18:40 -0600

    fix logic

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     completed = []
     for todo in todos:
-        if todo.get("completed"):
+        if todo.get("completed") == True:
             completed.append(todo.get("title"))
 
     print(f"Employee {user.get('name')} is done with tasks"
