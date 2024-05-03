610b79c4f605d53f0c5d5af9899eeb0e8e9db65b
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-05-03 02:45:25 -0600

    new test

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,52 +1,20 @@
 #!/usr/bin/python3
-"""returns information about his/her todo list progress.
-"""
+"""Import Modules"""
+import csv
 import requests
-import sys
+from sys import argv
 
+if __name__ == '__main__':
+    url_users = requests.get('https://jsonplaceholder.typicode.com/users')
+    url_todos = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-def Gather_data(arg1):
-    base_url = "https://jsonplaceholder.typicode.com/"
-    response_users = requests.get(
-        f"{base_url}/users"
-        )
-    response_users_json = response_users.json()
-    listofids = []
-    for index in range(len(response_users_json)):
-        for key in response_users_json[index]:
-            if key == "id":
-                listofids.append(
-                    response_users_json[index]["id"]
-                )
-
-    if arg1 in listofids:
-        TOTAL_NUMBER_OF_TASKS = 0 
-        NUMBER_OF_DONE_TASKS = 0
-        response_todos = requests.get(f"{base_url}/todos")
-        response_todos_json = response_todos.json()
-        for index in range(len(response_users_json)):
-            if response_users_json[index]["id"] == arg1:
-                EMPLOYEE_NAME = response_users_json[index][
-                    "name"
-                ]
-
-        for index in range(len(response_todos_json)):
-            if response_todos_json[index]["userId"] == arg1:
-                TOTAL_NUMBER_OF_TASKS += 1
-                if response_todos_json[index]["completed"] is True:
-                    NUMBER_OF_DONE_TASKS += 1
-        print(
-            f"Employee {EMPLOYEE_NAME} is \
-done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
-
-        for index in range(len(response_todos_json)):
-            if response_todos_json[index]["userId"] == arg1:
-                if response_todos_json[index]["completed"] is True:
-                    TASK_TITLE = f"\t {response_todos_json[index]['title']}"
-                    print(TASK_TITLE)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        arg1 = int(sys.argv[1])
-        Gather_data(arg1)
+    for i in url_users.json():
+        if i['id'] == int(argv[1]):
+            username = i['username']
+    with open(f"{argv[1]}.csv", 'w') as f:
+        for i in url_todos.json():
+            if i['userId'] == int(argv[1]):
+                completed = i['completed']
+                title = i['title']
+                f.write("\"{}\",\"{}\",\"{}\",\"{}\"\n".
+                        format(argv[1], username, completed, title))
--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,47 +1,14 @@
 #!/usr/bin/python3
-"""extend Python script to export data in the CSV format
-"""
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-
-def export_to_csv(USER_ID):
-    base_url = "https://jsonplaceholder.typicode.com/"
-    response_users = requests.get(
-        f"{base_url}/users"
-        )
-    response_users_json = response_users.json()
-    listofids = []
-    for index in range(len(response_users_json)):
-        for key in response_users_json[index]:
-            if key == "id":
-                listofids.append(
-                    response_users_json[index]["id"]
-                )
-
-    USER_ID = int(sys.argv[1])
-    if USER_ID in listofids:
-        response_todos = requests.get(f"{base_url}/todos")
-        response_todos_json = response_todos.json()
-        for index in range(len(response_users_json)):
-            if response_users_json[index]["id"] == USER_ID:
-                USERNAME = response_users_json[index]["username"]
-
-        with open(f"{USER_ID}.csv", "w", newline="") as file:
-
-            for index in range(len(response_todos_json)):
-                if response_todos_json[index]["userId"] == USER_ID:
-                    rtjson = response_todos_json[index]
-                    TASK_COMPLETED_STATUS = rtjson["completed"]
-                    TASK_TITLE = response_todos_json[index]["title"]
-                    file.write(
-                        f'"{USER_ID}","{USERNAME}",\
-"{TASK_COMPLETED_STATUS}","{TASK_TITLE}"\n'
-                    )
-        file.close()
-
-
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        USER_ID = int(sys.argv[1])
-        export_to_csv(USER_ID)
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
